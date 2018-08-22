51aad2dbbdc642b4c51b83192fdabe5c39d10c2c
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2018-08-21 23:01:27 -0400

    updated coloring and spacing for header/cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import 'tachyons';
 
 const Card = ({ name, email, id}) => {
     return (
-        <div className="tc bg-light-green dib br3 pa0 ma3 dim">
+        <div className="tc bg-navy dib br3 pa0 ma3 dim white">
             <img src={`https://picsum.photos/200/?random`} alt="robofriend"/>
             <div>
                 <h2>Bar Name</h2>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <div className="tc bg-navy pv5 white">
-            <h1 className="bg-navy ma0"><code>Your City Here</code> Happy Hours!</h1>
-            <p className="bg-navy ma0">The TOP Happy Hour Deals all over the city!</p>
+        <div className="tc bg-navy pv5 yellow">
+            <h1 className="bg-navy f1"><code>[Your City HERE]</code> Happy Hours!</h1>
+            <p className="bg-navy pt4">The TOP Happy Hour Deals all over the city!</p>
         </div>
     );
 }
