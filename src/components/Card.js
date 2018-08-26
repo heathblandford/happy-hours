ad4e2455efd8bdd6a31cb40356ad9fb984a1f317
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2018-08-26 18:06:02 -0400

    added padding to special p tag, edited specials for Che

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,12 +12,12 @@ class Card extends Component {
         // }
 
         return (
-            <div className="tc bg-navy dib br3 pa0 ma3 w-25 grow white">
+            <div className="tc bg-navy dib br3 pa0 ma3 w5 grow white">
                 <img src={`https://picsum.photos/400/?${id}`} alt="bar"/>
                 <div>
                     <h2>{barName}</h2>
                     <h5>{neighborhood}</h5>
-                    <p className="h4">{!special ? specialsMessage : special}</p>
+                    <p className="h4 ph3">{!special ? specialsMessage : special}</p>
                     <p>{number}</p>
                 </div>
             </div>
