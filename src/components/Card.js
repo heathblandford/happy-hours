0386c7e7a5a4da8cabd04abeda54eb05a26626bd
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2018-08-27 19:07:04 -0400

    updated Card component. build for deployment 001

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,23 +2,16 @@ import React, { Component } from 'react';
 
 class Card extends Component {
     render() {
-        const { barName, number, neighborhood, id, special } = this.props;
+        const { barName, neighborhood, id, special } = this.props;
         let specialsMessage = "Looks like we don't have info on the happy hour special here. Send us a note to have it updated!";
-        // let specialDay = '';
-        // if(!{special}){
-        //     specialDay = <p>"Looks like we don't have info on the happy hour special here. Send us a note to have it updated!"</p>;
-        // } else {
-        //     specialDay = {special};
-        // }
 
         return (
             <div className="tc bg-navy dib br3 pa0 ma3 w5 grow white">
                 <img src={`https://picsum.photos/400/?${id}`} alt="bar"/>
                 <div>
-                    <h2>{barName}</h2>
+                    <h2 className="h3">{barName}</h2>
                     <h5>{neighborhood}</h5>
                     <p className="h4 ph3">{!special ? specialsMessage : special}</p>
-                    <p>{number}</p>
                 </div>
             </div>
         )
