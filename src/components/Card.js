3c18cf430b0175c0498ae107bdf571c1901397f1
heath blandford <35548666+heathblandford@users.noreply.github.com>
2018-08-31 11:07:01 -0400

    comment out img for speed

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ class Card extends Component {
 
         return (
             <div className="tc bg-navy br3 pa0 dib mw5 ma3 grow white outline">
-                //<img src={`https://picsum.photos/300/?${id}`} alt="bar"/>
+            {/*<img src={`https://picsum.photos/300/?${id}`} alt="bar"/>*/}
                 <h2 className="f4 h3">{barName}</h2>
                 <h5>Neighborhood: {neighborhood}</h5>
                 <p className="f6 pa3 h4">{!special ? specialsMessage : special}</p>
