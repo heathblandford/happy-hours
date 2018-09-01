4fc6cb44f5ff14e7676104a4e2aa678f4b9cb9b8
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2018-08-31 20:45:21 -0400

    work on formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ const Card = ({ barName, neighborhood, special, id }) => {
     return(
         <div className="tc bg-navy br3 pa0 dib mw5 ma3 grow white outline">
         {/*<img src={`https://picsum.photos/300/?${id}`} alt="bar"/>*/}
-            <h2 className="f4 h3">{barName}</h2>
+            <h2 className="f4 pv3">{barName}</h2>
             <h5>Neighborhood: {neighborhood}</h5>
-            <p className="f6 pa3 h4">{!special ? "Looks like we don't have info on the happy hour special here. Send us a note to have it updated!" : special}</p>
+            <p className="f6 pa2">{!special ? "Looks like we don't have info on the happy hour special here. Send us a note to have it updated!" : special}</p>
         </div>
     )
 }
