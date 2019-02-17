bb7f56a18abd1b74a6b6eb1186313659a512b11e
Heath Blandford - Home PC <heath.blandford@gmail.com>
2019-02-17 18:28:07 -0500

    change card.js from functional component to class component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import '../Stylizers/card.css'
+import React from "react";
+import "../Stylizers/card.css";
 
-const Card = ({ barName, neighborhood, special, id }) => {
-    return(
-        <div className="Card">
-            <img src={`https://picsum.photos/100/?${id}`} alt="bar" className="responsiveImg"/>
-            <h2 className="barName">{barName}</h2>
-            <hr className="style-two" />
-            <h5 className="neighborhood">Neighborhood: {neighborhood}</h5>
-            <p className="special">{!special ? "Looks like we don't have info on the happy hour special here. Send us a note to have it updated!" : special}</p>
-        </div>
-    )
+class Card extends React.Component {
+  render() {
+    const { barName, neighborhood, special, id } = this.props;
+
+    return (
+      <div className="Card">
+        <img
+          src={`https://picsum.photos/100/?${id}`}
+          alt="bar"
+          className="responsiveImg"
+        />
+        <h2 className="barName">{barName}</h2>
+        <hr className="style-two" />
+        <h5 className="neighborhood">Neighborhood: {neighborhood}</h5>
+        <p className="special">
+          {!special
+            ? "Looks like we don't have info on the happy hour special here. Send us a note to have it updated!"
+            : special}
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Card;
+
