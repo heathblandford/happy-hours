3b0e98697891c48196493d18d3fa2ee63666f7b7
Heath Blandford - Home PC <heath.blandford@gmail.com>
2019-02-17 18:57:14 -0500

    comment out random image change image height/width

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,11 +7,11 @@ class Card extends React.Component {
 
     return (
       <div className="Card">
-        <img
-          src={`https://picsum.photos/100/?${id}`}
+        {/* <img
+          src={`https://picsum.photos/400/200/?${id}`}
           alt="bar"
           className="responsiveImg"
-        />
+        /> */}
         <h2 className="barName">{barName}</h2>
         <hr className="style-two" />
         <h5 className="neighborhood">Neighborhood: {neighborhood}</h5>
@@ -20,6 +20,7 @@ class Card extends React.Component {
             ? "Looks like we don't have info on the happy hour special here. Send us a note to have it updated!"
             : special}
         </p>
+        {/* <p className="expand">click to see more</p> */}
       </div>
     );
   }
