3bf2fde7352c408c9a4f613306282d5a72eea46e
Heath Blandford <heathblandford@Heaths-MacBook-Pro.local>
2019-02-26 22:09:19 -0500

    update barlist for formatting errors, change card height

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import '../Stylizers/searchbox.css';
+import React from "react";
+import "../Stylizers/searchbox.css";
 
-const Searchbox = ({ searchfield, searchChange}) => {
-    return (
-        <div className="Searchbox">
-            <input
-                type="search"
-                placeholder="Search Bars"
-                onChange={searchChange}/>
-        </div>
-    );
-}
+const Searchbox = ({ searchfield, searchChange }) => {
+  return (
+    <div className="Searchbox">
+      <input type="search" placeholder="Search Bars" onChange={searchChange}/>
+    </div>
+  );
+};
 
 export default Searchbox;
