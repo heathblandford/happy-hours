d023ad48f1ebb2ac1dafb87255182235a6dcc74e
heath-laptop <heath.blandford@gmail.com>
2019-08-12 20:22:24 -0400

    align landing buttons, make mobile friendly

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -9,18 +9,22 @@ class Landing extends Component{
       <div className='main'>
         <h1 className="title-baby">Pour Decisions</h1>
         <h5 className="slogan">All of the Best Happy Hour Deals</h5>
-        <Link to="/deals">
-          <button className="btn">See Deals</button>
-        </Link>
-        <Link to="/add-a-deal">
-          <button className="btn">Quick Add a Deal</button>
-        </Link>
-        <Link to="/contact">
-          <button className="btn">Contact Us</button>
-        </Link>
-        <Link to="/about">
-          <button className="btn">About Us</button>
-        </Link>
+        <div className="see-deal">
+          <Link to="/deals">
+            <button className="btn btn-deals">See Deals</button>
+          </Link>
+        </div>
+        <div className='links'>
+          <Link to="/add-a-deal">
+            <button className="btn">Quick Add a Deal</button>
+          </Link>
+          <Link to="/contact">
+            <button className="btn">Contact Us</button>
+          </Link>
+          <Link to="/about">
+            <button className="btn">About Us</button>
+          </Link>
+        </div>
 
         <p>Photo by Adam Wilson on Unsplash</p>
       </div>
